5eb70cf3ec08e4438823cdf204384433b043cbaa
Alejandro <alejandro12850mj@gmail.com>
2025-04-30 22:10:46 -0400

    exercise 2.4

--- part2/src/components/Course.jsx
+++ part2/src/components/Course.jsx
@@ -1,33 +1,34 @@
 
-export function Course({ course }) { 
+export function Course({ course }) {
 
-const Header = (props) => <h1>{props.course}</h1>
+    const Header = (props) => <h1>{props.course}</h1>
 
-const Content = (props) => (
-  <div>
-    <Part part={props.parts[0]} />
-    <Part part={props.parts[1]} />
-    <Part part={props.parts[2]} />
-  </div>
-)
+    const Content = (props) => (
+        <div>
+          
+            {props.parts.map((part) => (
+                <Part key={part.id} part={part} />
+            ))}
+        </div>
+    )
 
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
+    const Part = (props) => (
+        <p>   
+            {props.part?.name} {props.part?.exercises}
+        </p>
+    )
 
-const Total = (props) => <p>Number of exercises {props.total}</p>
+    const Total = (props) => <p>Number of exercises {props.total}</p>
 
     return (
         <div>
-        <Header course={course.name} />
-        <Content parts={course.parts} />
-        <Total
-          total={
-            course.parts.reduce((accumulator, part) => accumulator + part.exercises, 0)
-          }
-        />
-      </div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total
+                total={
+                    course.parts.reduce((accumulator, part) => accumulator + part.exercises, 0)
+                }
+            />
+        </div>
     )
 }

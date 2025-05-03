90abe1f9832736c9f05f19b8152841c10cb12d06
Alejandro <alejandro12850mj@gmail.com>
2025-05-02 22:52:56 -0400

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const FilterNames = ({onChange}) => {
   return (
@@ -50,12 +51,7 @@ const Persons = ({ persons, filter }) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState()
@@ -81,8 +77,18 @@ const App = () => {
     }
     const names = persons.concat({ name: newName, number: newNumber })
     setNewName('')
+    setNewNumber('')
     setPersons(names)
   }
+
+  useEffect(() => {
+    axios
+    .get('http://localhost:3001/persons')
+    .then((response) => {
+      setPersons(response.data)
+    })
+  }, [])
+
   return (
     <div>
       <h2>Phonebook</h2>

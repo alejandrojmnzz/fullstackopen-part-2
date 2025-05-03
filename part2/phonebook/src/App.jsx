9ace1254906d3912ec92f186e35dd74bbfcc01ca
Alejandro <alejandro12850mj@gmail.com>
2025-05-03 19:24:12 -0400

    exercise 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -75,7 +75,13 @@ const App = () => {
       setNewName('')
       return
     }
-    const names = persons.concat({ name: newName, number: newNumber })
+
+    axios
+    .post('http://localhost:3001/persons', {name: newName, number: newNumber})
+    .then((response) => {
+      setPersons(persons.concat(response.data))
+    })
+
     setNewName('')
     setNewNumber('')
     setPersons(names)
